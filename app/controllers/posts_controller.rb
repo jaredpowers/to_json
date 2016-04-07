266440b56fcab622f2febb49539c841e4a258723
JaredPowers <jarpow44@gmail.com>
2016-04-07 16:15:47 -0500

    finished controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,37 @@ class PostsController < ApplicationController
   end
 
   def show
+    post = Post.find(params[:id])
+    if post
+      @post = postend
+    end
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.body = params[:post][:body]
+    @post.save
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+private
+
+def post_params
+  params.required(:post).permit(:body, :title)
 end
